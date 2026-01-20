2b0a42a30beb93b1516c689fda323b496cad9a86
wanderlustayush <164509203+wanderlustayush@users.noreply.github.com>
2026-01-20 14:55:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const server=http.createServer(app)
 const io=new Server(server,{
 
     cors:{
-    origin:"http://localhost:5173",
+    origin:"https://vingo-p6ag.onrender.com",
     credentials:true,
     methods:['POST',"GET"]
 }
@@ -36,7 +36,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000;
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vingo-p6ag.onrender.com",
     credentials:true
 }))
 app.use(express.json())
